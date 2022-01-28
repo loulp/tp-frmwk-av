b39837e25a373c5447264a18c8d325662f754c0d
loulp <louislpogam@orange.fr>
2022-01-28 10:22:01 +0100

    first commit

---
+++ back-tp/src/main/java/framework/tp/backtp/BackTpApplication.java
@@ -0,0 +1,13 @@
+package framework.tp.backtp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackTpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackTpApplication.class, args);
+	}
+
+}
